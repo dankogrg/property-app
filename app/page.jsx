8a5ff64a222bdb138d13a71459d2ebbf38db1582
dankogrg <dankogrg@gmail.com>
2024-05-09 22:48:09 +0200

    dropmenus

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
             <h1 className="text-3xl">Welcome</h1>
             <Link href="/properties">Show Properties</Link>
         </div>
-    )
+    );
 };
 
 export default HomePage;
