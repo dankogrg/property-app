226b9e3efbfd6737395cc01009a368e5c8ed665e
dankogrg <dankogrg@gmail.com>
2024-05-17 11:16:26 +0200

    create property and upload images

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,7 @@
 
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyImages from "@/components/PropertyImages";
 import Spinner from "@/components/Spinner";
 import { fetchProperty } from "@/utils/request";
 import Link from "next/link";
@@ -151,6 +152,7 @@ const PropertyPage = () => {
                             </div>
                         </div>
                     </section>
+                    <PropertyImages images={property.images} />
                 </>
             )}
         </>
