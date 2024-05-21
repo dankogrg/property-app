38e80903681f9cb5a364a9be1f18192916fdbdfd
dankogrg <dankogrg@gmail.com>
2024-05-21 22:10:02 +0200

    bugfix

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -2,18 +2,13 @@ import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 
 export const getSessionUser = async () => {
-    try {
-        const session = await getServerSession(authOptions);
-        if (!session || !session.user) {
-            return null;
-        }
-
-        return {
-            user: session.user,
-            userId: session.user.id,
-        };
-    } catch (error) {
-        console.error(error);
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user) {
         return null;
     }
+
+    return {
+        user: session.user,
+        userId: session.user.id,
+    };
 };
